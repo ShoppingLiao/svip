dc6b6f3b3ff86acf533d08fe3f0bf857c9cae014
ShoppingLiao <tom0801a@gmail.com>
2020-05-26 20:41:44 +0800

    Updates

---
+++ precache-manifest.148f733aa8eefed41af1be4764166109.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2348f7bafae3fb35a9ad712c6b7df38b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd5ff2ef7deb4cd1eb07",
+    "url": "/static/css/main.ddce0106.chunk.css"
+  },
+  {
+    "revision": "122f5e16e81d42e11dcc",
+    "url": "/static/js/2.6dcfa552.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6dcfa552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd5ff2ef7deb4cd1eb07",
+    "url": "/static/js/main.6f3f6b21.chunk.js"
+  },
+  {
+    "revision": "fab75bfaeaef051cf4a0",
+    "url": "/static/js/runtime-main.a156eb14.js"
+  },
+  {
+    "revision": "491af00caff85ce9d983eefd65cc6a02",
+    "url": "/static/media/2.491af00c.png"
+  },
+  {
+    "revision": "b56a15690769b5d6e240f91630d69406",
+    "url": "/static/media/4.b56a1569.png"
+  },
+  {
+    "revision": "88af6033860b147346b45433b9bba4bd",
+    "url": "/static/media/bg.88af6033.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6c54a36fb83eec0d6ea44a55a4612985",
+    "url": "/static/media/title.6c54a36f.png"
+  }
+]);
