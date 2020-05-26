dd74d964481431142b95b88b6c34480717844f52
ShoppingLiao <tom0801a@gmail.com>
2020-05-26 21:43:31 +0800

    feat: 改成RWD

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <img alt="pic" src={require('../src/images/4.png')} />
-      <img alt="pic" src={require('../src/images/title.png')} />
+      <img className="header" alt="pic" src={require('../src/images/4.png')} />
+      <img
+        className="title"
+        alt="pic"
+        src={require('../src/images/title.png')}
+      />
       <a href="http://www-svipky.com/">點擊進入官網</a>
-      <img alt="pic" src={require('../src/images/2.png')} />
+      <img className="footer" alt="pic" src={require('../src/images/2.png')} />
     </div>
   );
 }
